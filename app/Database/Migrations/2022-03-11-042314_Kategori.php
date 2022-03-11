149c88f7f513709385b3af87eb4af008d55c6534
aqilmustaqim <aqilmustaqim28@gmail.com>
2022-03-11 11:37:35 +0700

    Membuat Migrations Database Kategori

--- app/Database/Migrations/2022-03-11-042314_Kategori.php
+++ app/Database/Migrations/2022-03-11-042314_Kategori.php
@@ -8,11 +8,26 @@ class Kategori extends Migration
 {
     public function up()
     {
-        //
+        //id,kategori
+
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'kategori'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '128'
+            ],
+        ]);
+        $this->forge->addKey('id', TRUE);
+        $this->forge->createTable('kategori');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('kategori');
     }
 }
