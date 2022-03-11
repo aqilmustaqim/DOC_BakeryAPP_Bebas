1c6a00ce65b96ba863eea99014507d96f82b063c
aqilmustaqim <aqilmustaqim28@gmail.com>
2022-03-11 12:57:55 +0700

    Menambahkan Migrations Database Tabel PenjualanDetail

--- app/Database/Migrations/2022-03-11-055521_PenjualanDetail.php
+++ app/Database/Migrations/2022-03-11-055521_PenjualanDetail.php
@@ -8,11 +8,46 @@ class PenjualanDetail extends Migration
 {
     public function up()
     {
-        //
+        //id, invoice, kode_produk, harga_beli , harga_jual , jumlah, subtotal
+
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE
+            ],
+            'invoice'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '128'
+            ],
+            'kode_produk' => [
+                'type'           => 'INT',
+                'constraint'     => '128'
+            ],
+            'harga_beli' => [
+                'type'           => 'INT',
+                'constraint'     => '128'
+            ],
+            'harga_jual' => [
+                'type'           => 'INT',
+                'constraint'     => '128'
+            ],
+            'jumlah' => [
+                'type'           => 'INT',
+                'constraint'     => '128'
+            ],
+            'subtotal' => [
+                'type'           => 'INT',
+                'constraint'     => '128'
+            ],
+        ]);
+        $this->forge->addKey('id', TRUE);
+        $this->forge->createTable('penjualan_detail');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('penjualan_detail');
     }
 }
