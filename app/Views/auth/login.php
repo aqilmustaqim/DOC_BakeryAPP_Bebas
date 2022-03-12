36d41f3e4e9452930e040d4f03df20dcfbc8b35e
aqilmustaqim <aqilmustaqim28@gmail.com>
2022-03-12 19:12:20 +0700

    Membuat Fungsi Login dengan Session

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -10,31 +10,26 @@
                             <div class="auth-form">
                                 <h4 class="text-center mb-4">Login <b>BakeryAPP</b></h4>
 
-                                <?php //if (session()->getFlashData('login')) : 
+                                <?php if (session()->getFlashData('login')) :
                                 ?>
-                                <div class="alert alert-danger solid alert-dismissible fade show">
-                                    <svg viewBox="0 0 24 24" width="24 " height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="mr-2">
-                                        <polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon>
-                                        <line x1="15" y1="9" x2="9" y2="15"></line>
-                                        <line x1="9" y1="9" x2="15" y2="15"></line>
-                                    </svg>
-                                    <strong>Error!</strong> <? //= session()->getFlashdata('login'); 
-                                                            ?>.
-                                    <button type="button" class="close h-100" data-dismiss="alert" aria-label="Close"><span><i class="mdi mdi-close"></i></span>
-                                    </button>
-                                </div>
-                                <?php //endif; 
-                                ?>
-                                <?php //if (session()->getFlashData('forgotSuccess')) : 
-                                ?>
-                                <div class="alert alert-success solid alert-dismissible fade show">
-                                    <strong>Success!</strong> <? //= session()->getFlashdata('forgotSuccess'); 
-                                                                ?>.
-                                    <button type="button" class="close h-100" data-dismiss="alert" aria-label="Close"><span><i class="mdi mdi-close"></i></span>
-                                    </button>
-                                </div>
-                                <?php //endif; 
-                                ?>
+                                    <div class="alert alert-danger solid alert-dismissible fade show">
+                                        <svg viewBox="0 0 24 24" width="24 " height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="mr-2">
+                                            <polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon>
+                                            <line x1="15" y1="9" x2="9" y2="15"></line>
+                                            <line x1="9" y1="9" x2="15" y2="15"></line>
+                                        </svg>
+                                        <strong>Error!</strong> <?= session()->getFlashdata('login'); ?>
+                                        <button type="button" class="close h-100" data-dismiss="alert" aria-label="Close"><span><i class="mdi mdi-close"></i></span>
+                                        </button>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if (session()->getFlashData('forgotSuccess')) : ?>
+                                    <div class="alert alert-success solid alert-dismissible fade show">
+                                        <strong>Success!</strong> <?= session()->getFlashdata('forgotSuccess');  ?>
+                                        <button type="button" class="close h-100" data-dismiss="alert" aria-label="Close"><span><i class="mdi mdi-close"></i></span>
+                                        </button>
+                                    </div>
+                                <?php endif; ?>
 
 
                                 <form method="POST" action="<?= base_url(); ?>/auth/loginSave">
