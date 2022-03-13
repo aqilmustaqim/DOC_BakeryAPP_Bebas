bea8745cb041a9c8d4a26d42c9971432c0ce0873
aqilmustaqim <aqilmustaqim28@gmail.com>
2022-03-13 13:08:51 +0700

    Menambah Logo Dan Text Logo

--- app/Views/templates/navheader.php
+++ app/Views/templates/navheader.php
@@ -3,9 +3,9 @@
         ***********************************-->
 <div class="nav-header">
     <a href="index.html" class="brand-logo">
-        <img class="logo-abbr" src="<?= base_url('assets'); ?>/images/logo.png" alt="">
-        <img class="logo-compact" src="<?= base_url('assets'); ?>/images/text-logo.png" alt="">
-        <img class="brand-title" src="<?= base_url('assets'); ?>/images/text-logo.png" alt="">
+        <img class="logo-abbr" src="<?= base_url('assets'); ?>/images/logobakery.png" alt="">
+        <img class="logo-compact" src="<?= base_url('assets'); ?>/images/textbakery2.png" alt="">
+        <img class="brand-title" src="<?= base_url('assets'); ?>/images/textbakery2.png" alt="">
     </a>
 
     <div class="nav-control">
