0e0a16aab32f9ef36898aa6ecf0cfd0879e4734e
aqilmustaqim <aqilmustaqim28@gmail.com>
2022-03-20 21:12:52 +0700

    Membuat Controller Master

---
+++ app/Controllers/Master.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+class Master extends BaseController
+{
+
+    public function produk()
+    {
+        //1.Cek Login
+        if (!session()->has('logged_in')) {
+            //Jika tidak ada user yang login maka tendang
+            //Login Dulu Kau
+            session()->setFlashdata('login', 'Silahkan Login Terlebih Dahulu !'); //Buat FlashData
+            return redirect()->to(base_url()); // Arahkan ke halaman login
+        } else {
+            //Kalau sudah ada usernya cek apakah user itu admin ?
+            if (session()->get('role_id') != 1) {
+                //Kalau role id nya bukan 1 alias bukan admin berarti dia kasir
+                return redirect()->to(base_url('kasir')); // Arahkan ke halaman kasir
+            }
+        }
+        //Membuat Halaman Utama Produk
+        $data = [
+            'title' => 'BakeryAPP || Produk',
+            'validation' => \Config\Services::validation()
+        ];
+
+        //Arahkan Ke Views Produk
+        return view('master/produk', $data);
+    }
+}
