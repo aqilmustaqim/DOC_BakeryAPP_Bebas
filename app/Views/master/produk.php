970ce28afb733d520937ea0d187d7aeea1ca1401
aqilmustaqim <aqilmustaqim28@gmail.com>
2022-03-20 21:13:22 +0700

    Membuat Views Produk

---
+++ app/Views/master/produk.php
@@ -0,0 +1,17 @@
+<?= $this->extend('templates/index'); ?>
+<?= $this->section('content'); ?>
+
+<div class="content-body">
+    <!-- row -->
+    <div class="container-fluid">
+        <div class="form-head d-flex mb-3 align-items-start">
+            <div class="mr-auto d-none d-lg-block">
+                <h2 class="text-black font-w600 mb-0">Produk</h2>
+                <p class="mb-0">Halaman Produk Bosss</p>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
